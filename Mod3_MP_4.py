295ce203db8d6283cc3c569dd13ef3e02346652e
Kulas <202311397@fit.edu.ph>
2025-05-07 16:28:56 +0800

    Update Mod3_MP_4.py

--- Mod3_MP_4.py
+++ Mod3_MP_4.py
@@ -1,26 +1,27 @@
 import random
 
 print ("Welcome to numbers guessing game\nMenu: \n1. Play\n2. Exit")
-random_number = random.randint(1, 10)
 
-choice = int(input("input your choice: "))
+
+
 
 while True:
+    choice = int(input("input your choice: "))
     if choice == 1:
+        random_number = random.randint(1, 10)
         guess = None
         counter = 1
         while guess!=random_number:
             guess = int(input("Kindly input your guess: "))
             if guess > random_number:
-                print ("Guess Lower")
+                print ("Lower!")
                 counter+=1
             elif guess < random_number:
-                print ("Guess Higher")
-                counter+=1
-
-        else:
-            print(f"You got it right in just {counter} tries")
-            break
+                print ("Higher!")
+                counter+=1     
+        print(f"You got it right in just {counter} tries")
+        break
+        
 
     elif choice == 2:
         break
